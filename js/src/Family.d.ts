72472c558b17c55d4c2c0602f719a2ed2e80e66b
Kevin Pei <kevin-pei@hotmail.com>
2017-05-15 23:05:50 -0700

    remove random files

--- js/src/Family.d.ts
+++
@@ -1,8 +0,0 @@
-import { Person } from './Person';
-export declare class Family {
-    name: string;
-    members: Person[];
-    constructor(name: string);
-    addMember(person: Person): void;
-    readonly size: number;
-}
--- js/src/Person.d.ts
+++
@@ -1,9 +0,0 @@
-import { Family } from './Family';
-export declare class Person {
-    name: string;
-    family: Family;
-    constructor(name: string);
-    sayHello(): string;
-    setFamily(family: Family): void;
-    readonly fullName: string;
-}
